59d2f0bdb8910f3b876882980b9652afd155b06f
Daniel Costa <dpcosta@gmail.com>
2020-02-08 10:51:14 -0300

    Video 2.2
    Mais de um desconto

--- src/CalculadoraDeDescontos.php
+++ src/CalculadoraDeDescontos.php
@@ -10,6 +10,10 @@ class CalculadoraDeDescontos
             return $orcamento->valor * 0.1;
         }
 
+        if ($orcamento->valor > 500) {
+            return $orcamento->valor * 0.05;
+        }
+
         return 0;
     }
 }
--- testes.php
+++ testes.php
@@ -17,7 +17,7 @@ echo $calculadora->calcula($orcamento, new Iss());
 $calculadora = new CalculadoraDeDescontos();
 
 $orcamento = new Orcamento();
-$orcamento->valor = 200;
-$orcamento->quantidadeItens = 5;
+$orcamento->valor = 600;
+$orcamento->quantidadeItens = 6;
 
 echo $calculadora->calculaDescontos($orcamento);
