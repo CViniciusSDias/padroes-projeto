ca9414eef4fa3c23f37a89f4c47fe243ac1dc788
Daniel Costa <dpcosta@gmail.com>
2020-02-08 11:59:22 -0300

    Video 3.2
    Implementando Template Method

--- src/Impostos/Icpp.php
+++ src/Impostos/Icpp.php
@@ -4,14 +4,20 @@ namespace Alura\DesignPattern\Impostos;
 
 use Alura\DesignPattern\Orcamento;
 
-class Icpp implements Imposto
+class Icpp extends ImpostoCom2Aliquotas
 {
-    public function calculaImposto(Orcamento $orcamento): float
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
     {
-        if ($orcamento->valor > 500) {
-            return $orcamento->valor * 0.03;
-        }
+        return $orcamento->valor > 500;
+    }
 
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.03;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
         return $orcamento->valor * 0.02;
     }
 }
--- src/Impostos/Ikcv.php
+++ src/Impostos/Ikcv.php
@@ -4,14 +4,20 @@ namespace Alura\DesignPattern\Impostos;
 
 use Alura\DesignPattern\Orcamento;
 
-class Ikcv implements Imposto
+class Ikcv extends ImpostoCom2Aliquotas
 {
-    public function calculaImposto(Orcamento $orcamento): float
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
     {
-        if ($orcamento->valor > 300 && $orcamento->quantidadeItens > 3) {
-            return $orcamento->valor * 0.04;
-        }
+        return $orcamento->valor > 300 && $orcamento->quantidadeItens > 3;
+    }
 
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.04;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
         return $orcamento->valor * 0.025;
     }
 }
