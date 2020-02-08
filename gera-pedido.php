dd14a69e504d37bdbe188ea4f7e5570d037a1708
Daniel Costa <dpcosta@gmail.com>
2020-02-08 14:21:07 -0300

    Video 5.1
    Criando arquivo gera-pedido.php

---
+++ gera-pedido.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Alura\DesignPattern\{Orcamento, Pedido};
+
+$valorOrcamento = $argv[1];
+$numeroDeItens = $argv[2];
+$nomeCliente = $argv[3];
+
+$orcamento = new Orcamento();
+$orcamento->quantidadeItens = $numeroDeItens;
+$orcamento->valor = $valorOrcamento;
+
+$pedido = new Pedido();
+$pedido->dataFinalizacao = new \DateTimeImmutable();
+$pedido->nomeCliente = $nomeCliente;
+$pedido->orcamento = $orcamento;
+
+echo "Cria pedido no banco de dados " . PHP_EOL;
+echo "Envia e-mail para cliente " . PHP_EOL;
