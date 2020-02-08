571e4943e0fefcad8c807f3b29f35837746b2f61
Daniel Costa <dpcosta@gmail.com>
2020-02-08 12:16:47 -0300

    Video 4.1
    Implementando estados no orçamento

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -6,4 +6,25 @@ class Orcamento
 {
     public int $quantidadeItens;
     public float $valor;
+    public string $estadoAtual;
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->calculaDescontoExtra();
+    }
+
+    public function calculaDescontoExtra(): float
+    {
+        if ($this->estadoAtual == 'EM_APROVACAO') {
+            return $this->valor * 0.05;
+        }
+
+        if ($this->estadoAtual == 'APROVADO') {
+            return $this->valor * 0.02;
+        }
+
+        throw new \DomainException(
+            'Orçamentos reprovados e finalizados não podem receber desconto'
+        );
+    }
 }
