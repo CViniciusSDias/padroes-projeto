9be5eb49eff9fd88e33cf640051df1dab1ce534a
dpcosta <dpcosta@gmail.com>
2020-03-14 15:54:54 -0300

    Video 6.2
    Implementando Singleton

--- pdo.php
+++ pdo.php
@@ -4,9 +4,11 @@ use Alura\DesignPattern\PdoConnection;
 
 require 'vendor/autoload.php';
 
-$pdo = new PdoConnection('sqlite::memory:');
+$pdo = PdoConnection::getInstance('sqlite::memory:');
 // $pdo->query('');
 
-$pdo2 = new PdoConnection('sqlite::memory:');
+$pdo2 = PdoConnection::getInstance('sqlite::memory:');
+
+$pdo2->bindValue();
 
 var_dump($pdo, $pdo2);
