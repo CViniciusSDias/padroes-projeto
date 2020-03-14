2a507982b65a45a387c03b6f79e4d72b3325974f
dpcosta <dpcosta@gmail.com>
2020-03-14 11:29:07 -0300

    Video 4.1
    Criando classe NotaFiscal

---
+++ src/NotaFiscal/NotaFiscal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Alura\DesignPattern\NotaFiscal;
+
+class NotaFiscal
+{
+    public string $cnpjEmpresa;
+    public string $razaoSocialEmpresa;
+    public array $itens;
+    public string $observacoes;
+    public \DateTimeInterface $dataEmissao;
+    public float $valorImpostos;
+
+    public function valor(): float
+    {
+        return 0;
+    }
+}
