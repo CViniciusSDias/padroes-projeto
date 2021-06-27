62305397617b7d0d3839f9662ebf3e547565e787
Renato Augusto <renatoaugusto.ads@gmail.com>
2021-06-27 11:22:44 -0300

    FIX: Corrigindo classe Orcamento
    Descricao: Com o passar do tempo e com novos padrões de projeto sendo
    implementados, a classe Orcamento sofreu uma alteração que gerou uma
    falha no sistema. Antes ela possuia um atributo "valor", porém com
    as novas alterações a classe Orcamento passou a receber Objetos do
    tipo ItensOrcamento que possuiam o atriuto valor e com isso criamos o
    metodo Orcamento->valor() que calculava o valor de todos os itens daquele
    orçamento.
    
    Problema: Esse atributo Orcamento->valor ainda era utilizado em diversas
    classes do sistema, o que provocaria uma falha grave, pois não existe
    mais esse atributo.
    
    Solução: todas as classes que utilizavam o atributo valor do Orcamento
    foram alteradas para utilizar o método Orcamento->valor().
    
    OBS: o método Orcamento->aplicarDescontoExtra() foi alterado para que
    retornasse o valor do desconto baseado no valor do orçamento.
    Antes este método apenas setava o atributo valor que não existe mais.
    
    OBS: foi criado um arquivo de teste para verificar se tudo está se
    comportando como deveria (teste-aplicar-desconto-extra.php)

--- src/Descontos/DescontoMaisDe500Reais.php
+++ src/Descontos/DescontoMaisDe500Reais.php
@@ -8,8 +8,8 @@ class DescontoMaisDe500Reais extends Desconto
 {
     public function calculaDesconto(Orcamento $orcamento): float
     {
-        if ($orcamento->valor > 500) {
-            return $orcamento->valor * 0.05;
+        if ($orcamento->valor() > 500) {
+            return $orcamento->valor() * 0.05;
         }
 
         return $this->proximoDesconto->calculaDesconto($orcamento);
--- src/Impostos/Icpp.php
+++ src/Impostos/Icpp.php
@@ -8,16 +8,16 @@ class Icpp extends ImpostoCom2Aliquotas
 {
     protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
     {
-        return $orcamento->valor > 500;
+        return $orcamento->valor() > 500;
     }
 
     protected function calculaTaxaMaxima(Orcamento $orcamento): float
     {
-        return $orcamento->valor * 0.03;
+        return $orcamento->valor() * 0.03;
     }
 
     protected function calculaTaxaMinima(Orcamento $orcamento): float
     {
-        return $orcamento->valor * 0.02;
+        return $orcamento->valor() * 0.02;
     }
 }
